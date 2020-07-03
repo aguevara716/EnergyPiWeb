041c8e5c98b1b6e5c2c870d8c20e1ee0b078bea6
Angel Guevara, Jr <aguevara.716@gmail.com>
2020-07-03 18:17:47 -0400

    Added convenience methods to get the first day of the given month and the first day of the next month

---
+++ src/EnergyPi.Web/Extensions/DateTimeExtensions.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace EnergyPi.Web.Extensions
+{
+    public static class DateTimeExtensions
+    {
+        public static DateTime GetFirstDayOfMonth(DateTime date)
+        {
+            var firstDayOfMonth = new DateTime(date.Year, date.Month, 1);
+            return firstDayOfMonth;
+        }
+
+        public static DateTime GetFirstDayOfNextMonth(DateTime date)
+        {
+            var year = date.Year;
+            var nextMonth = date.Month + 1;
+
+            if (nextMonth == 13)
+            {
+                year += 1;
+                nextMonth = 1;
+            }
+
+            var firstDayOfNextMonth = new DateTime(year, nextMonth, 1);
+            return firstDayOfNextMonth;
+        }
+
+    }
+}
