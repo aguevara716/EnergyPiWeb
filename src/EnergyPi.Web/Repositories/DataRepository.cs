4ef0a84bcc15f80e2f2f8cbc002834f4a3c208ff
Angel Guevara, Jr <aguevara.716@gmail.com>
2020-07-03 18:17:04 -0400

    Added the IRepository<T> interface

---
+++ src/EnergyPi.Web/Repositories/DataRepository.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+using EnergyPi.Web.Data;
+using EnergyPi.Web.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace EnergyPi.Web.Repositories
+{
+    public class DataRepository<TEntity> : IRepository<TEntity>
+        where TEntity : class, IEntity
+    {
+        // Dependencies
+        private readonly DataDbContext _dataDbContext;
+
+        // Constructors
+        public DataRepository(DataDbContext dataDbContext)
+        {
+            _dataDbContext = dataDbContext;
+        }
+
+        // Private Methods
+
+        // Create
+        /* purposely blank */
+
+        // Read
+        public TEntity Get(int id)
+        {
+            var record = Get().FirstOrDefault(t => t.Id == id);
+            return record;
+        }
+
+        public Task<TEntity> GetAsync(int id)
+        {
+            var record = Get().FirstOrDefaultAsync(t => t.Id == id);
+            return record;
+        }
+
+        public IQueryable<TEntity> Get()
+        {
+            var table = _dataDbContext.Set<TEntity>();
+            return table;
+        }
+
+        public IQueryable<TEntity> Get(Func<TEntity, Boolean> predicate)
+        {
+            var results = Get().Where(predicate).AsQueryable();
+            return results;
+        }
+
+        // Update
+        /* purposely blank */
+
+        // Delete
+        /* purposely blank */
+
+    }
+}
---
+++ src/EnergyPi.Web/Repositories/IRepository.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+using EnergyPi.Web.Entities;
+
+namespace EnergyPi.Web.Repositories
+{
+    public interface IRepository<TEntity>
+        where TEntity : IEntity
+    {
+        // Create
+
+        // Read
+        TEntity Get(int id);
+        Task<TEntity> GetAsync(int id);
+        IQueryable<TEntity> Get();
+        IQueryable<TEntity> Get(Func<TEntity, Boolean> predicate);
+
+        // Update
+
+        // Delete
+    }
+}
