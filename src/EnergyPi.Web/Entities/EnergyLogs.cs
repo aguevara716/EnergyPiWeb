37511abc552e6672a45fe1beec4606d7eb88caa1
Angel Guevara, Jr <aguevara.716@gmail.com>
2020-07-03 18:16:48 -0400

    Added the IEntity interface

--- src/EnergyPi.Web/Entities/EnergyLogs.cs
+++ src/EnergyPi.Web/Entities/EnergyLogs.cs
@@ -1,9 +1,17 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace EnergyPi.Web.Entities
 {
-    public partial class EnergyLogs
+    public partial class EnergyLogs : IEntity
     {
+        [NotMapped]
+        public int Id
+        {
+            get { return EnergyLogId; }
+            set { EnergyLogId = value; }
+        }
+
         public int EnergyLogId { get; set; }
         public DateTime Timestamp { get; set; }
         public decimal? TotalConsumption { get; set; }
