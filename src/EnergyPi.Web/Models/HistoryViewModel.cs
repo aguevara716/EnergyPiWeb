6f29fdb702e49f9a505dbc0a48e0f40a93f52583
Angel Guevara, Jr <aguevara.716@gmail.com>
2020-07-06 00:56:46 -0400

    Implemented the HistoryViewModel & builder

--- src/EnergyPi.Web/Models/HistoryViewModel.cs
+++ src/EnergyPi.Web/Models/HistoryViewModel.cs
@@ -1,9 +1,18 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace EnergyPi.Web.Models
 {
     public class HistoryViewModel
     {
+        // Focused date
+        public DateTime FocusedDate { get; set; }
+        public decimal FocusedDaysTotalConsumption { get; set; }
+        public Dictionary<DateTime, Decimal?> FocusedDaysHourlyConsumption { get; set; }
+        public Dictionary<DateTime, Decimal> FocusedHourConsumption { get; set; }
 
+        // Month
+        public decimal FocusedMonthsTotalConsumption { get; set; }
+        public Dictionary<DateTime, Decimal?> FocusMonthsDailyConsumption { get; set; }
     }
 }
