11cfb1aa540e6f40d7fce56c7e22b912bee8d93f
Angel Guevara, Jr <angel.guevara@valiant.com>
2020-07-07 15:58:32 -0400

    Added weather graphs to the dashboard

---
+++ src/EnergyPi.Web/DataServices/WeatherLogsDataService.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Linq;
+using EnergyPi.Web.Entities;
+using EnergyPi.Web.Repositories;
+
+namespace EnergyPi.Web.DataServices
+{
+    public interface IWeatherLogsDataService
+    {
+        IQueryable<WeatherLogs> GetRecordsForDate(DateTime date);
+        IQueryable<WeatherLogs> GetRecordsForDateRange(DateTime startDate, DateTime endDate);
+    }
+
+    public class WeatherLogsDataService : IWeatherLogsDataService
+    {
+        // Dependencies
+        private readonly IRepository<WeatherLogs> _weatherLogsRepository;
+
+        // Constructors
+        public WeatherLogsDataService(IRepository<WeatherLogs> weatherLogsRepository)
+        {
+            _weatherLogsRepository = weatherLogsRepository;
+        }
+
+        // Get Records
+        public IQueryable<WeatherLogs> GetRecordsForDate(DateTime date)
+        {
+            var nextDay = date.Date.AddDays(1);
+
+            var records = GetRecordsForDateRange(date, nextDay);
+            return records;
+        }
+
+        public IQueryable<WeatherLogs> GetRecordsForDateRange(DateTime startDate, DateTime endDate)
+        {
+            var records = _weatherLogsRepository.Get(wl => wl.Timestamp.Date >= startDate.Date && wl.Timestamp.Date < endDate.Date);
+            return records;
+        }
+
+    }
+}
