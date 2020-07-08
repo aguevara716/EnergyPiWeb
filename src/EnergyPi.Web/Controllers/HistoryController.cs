9f39493df455844d5dc2c48e445338b56473c4fd
Angel Guevara, Jr <aguevara.716@gmail.com>
2020-07-08 00:52:17 -0400

    Added logic to display weather logs on the History page

--- src/EnergyPi.Web/Controllers/HistoryController.cs
+++ src/EnergyPi.Web/Controllers/HistoryController.cs
@@ -14,13 +14,16 @@ namespace EnergyPi.Web.Controllers
         // Dependencies
         private readonly IEnergyLogsDataService _energyLogsDataService;
         private readonly IHistoryViewModelBuilder _historyViewModelBuilder;
+        private readonly IWeatherLogsDataService _weatherLogsDataService;
 
         // Constructors
         public HistoryController(IEnergyLogsDataService energyLogsDataService,
-                                 IHistoryViewModelBuilder historyViewModelBuilder)
+                                 IHistoryViewModelBuilder historyViewModelBuilder,
+                                 IWeatherLogsDataService weatherLogsDataService)
         {
             _energyLogsDataService = energyLogsDataService;
             _historyViewModelBuilder = historyViewModelBuilder;
+            _weatherLogsDataService = weatherLogsDataService;
         }
 
         // Private Methods
@@ -30,7 +33,9 @@ namespace EnergyPi.Web.Controllers
             var firstDayOfNextMonth = DateTimeExtensions.GetFirstDayOfNextMonth(date);
 
             var energyLogs = _energyLogsDataService.GetRecordsForDateRange(firstDayOfMonth, firstDayOfNextMonth);
-            var historyViewModel = _historyViewModelBuilder.BuildViewModel(date, energyLogs);
+            var weatherLogs = _weatherLogsDataService.GetRecordsForDateRange(firstDayOfMonth, firstDayOfNextMonth);
+
+            var historyViewModel = _historyViewModelBuilder.BuildViewModel(date, energyLogs, weatherLogs);
             return historyViewModel;
         }
 
--- src/EnergyPi.Web/Models/HistoryViewModel.cs
+++ src/EnergyPi.Web/Models/HistoryViewModel.cs
@@ -5,6 +5,7 @@ namespace EnergyPi.Web.Models
 {
     public class HistoryViewModel
     {
+        /*Energy Logs*/
         // Focused date
         public DateTime FocusedDate { get; set; }
         public decimal FocusedDaysTotalConsumption { get; set; }
@@ -14,5 +15,22 @@ namespace EnergyPi.Web.Models
         // Month
         public decimal FocusedMonthsTotalConsumption { get; set; }
         public Dictionary<DateTime, Decimal?> FocusMonthsDailyConsumption { get; set; }
+
+        /*Weather Logs*/
+        public decimal FocusedDaysHighestTemperature { get; set; }
+        public decimal FocusedDaysAverageTemperature { get; set; }
+        public decimal FocusedDaysLowestTemperature { get; set; }
+
+        public decimal FocusedDaysHighestHumidity { get; set; }
+        public decimal FocusedDaysAverageHumidity { get; set; }
+        public decimal FocusedDaysLowestHumidity { get; set; }
+
+        public decimal FocusedHoursTemperature { get; set; }
+        public decimal FocusedHoursHumidity { get; set; }
+
+        public Dictionary<DateTime, Decimal?> FocusedDaysTemperature { get; set; }
+        public Dictionary<DateTime, Decimal?> FocusedMonthsHighestTemperature { get; set; }
+        public Dictionary<DateTime, Decimal?> FocusedMonthsAverageTemperature { get; set; }
+        public Dictionary<DateTime, Decimal?> FocusedMonthsLowestTemperature { get; set; }
     }
 }
